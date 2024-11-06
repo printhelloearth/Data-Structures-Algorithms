2ca97a0a0f04dddd7da15fcb850692c05414545d
Ej Zape <109931589+printhelloearth@users.noreply.github.com>
2024-11-06 09:59:50 +0800

    Add files via upload

---
+++ CompareNumbers.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._hoa_01;
+
+/**
+ *
+ * @author jocel
+ */
+import java.util.*;
+
+public class CompareNumbers 
+{
+    public static void main(String[] args) 
+    {
+       compareNumbers();
+    }
+    public static void compareNumbers()
+    {
+        Scanner scanner = new Scanner(System.in);
+  
+      System.out.print("Enter the first number: ");
+      double firstNumber = scanner.nextDouble();
+  
+      System.out.print("Enter the second number: ");
+      double secondNumber = scanner.nextDouble();
+  
+      if (firstNumber < secondNumber) {
+        System.out.println(firstNumber + " is less than " + secondNumber + ".");
+      } else if (firstNumber > secondNumber) {
+        System.out.println(firstNumber + " is greater than " + secondNumber + ".");
+      } else {
+        System.out.println(firstNumber + " is equal to " + secondNumber + ".");
+      }
+    }
+}
